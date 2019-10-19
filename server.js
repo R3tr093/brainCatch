3c055bacd6867b90a37166f08c5050eb9976ef81
Retro <mossiat.jeoffrey@outlook.com>
2019-10-19 13:36:54 +0200

    End deployement

--- server.js
+++ server.js
@@ -1,6 +1,79 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/brainCatch'));
-app.get('/*', function(req, res) {res.sendFile(path.join(__dirname + '/dist/brainCatch/index.html'));});
-app.listen(process.env.PORT || 3000); 
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var USERS_COLLECTION = "users";
+
+var app = express();
+
+
+
+app.use(bodyParser.json());
+
+
+// EDIT NAME
+var distDir = __dirname + "/dist/brainCatch";
+app.use(express.static(distDir));
+
+var db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+
+app.get("/api/users", function(req, res) {
+  db.collection(USERS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/contacts", function(req, res) {
+  var newUser = req.body;
+  newUser.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  } else {
+    db.collection(USERS_COLLECTION).insertOne(newUser, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new contact.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
+});
+
+
+app.get("/api/users/:id", function(req, res) {
+});
+
+app.put("/api/users/:id", function(req, res) {
+});
+
+app.delete("/api/users/:id", function(req, res) {
+});
