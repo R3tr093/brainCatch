a1575824c65ef9b05c0411ef4227b05002cb484b
Retro <mossiat.jeoffrey@outlook.com>
2019-10-21 16:47:02 +0200

    Add : rework design implement toogle forms

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,15 +12,63 @@ import { UsersServicesService } from '../services/users.service';
 })
 export class HomeComponent implements OnInit {
 
-
+  formOnScreen : boolean = false;
 
   constructor(private userService : UsersServicesService) { }
 
   ngOnInit() {
 
+
+    
+
+
       //this.userService.getUser("mLab Support");
 
      //this.userService.postUser({"name":"mLab Support", "email": "support@mlab.com", "password": "password"});
+  }
+
+  displayAuth()
+  {
+
+    if(!this.formOnScreen)
+    {
+      document.getElementById('authForm').classList.remove("slideOutDown");
+      document.getElementById("authForm").classList.add('slideInDown');
+      document.getElementById("authForm").style.display ="block";
+      this.formOnScreen = true;
+    }
+
+    else
+    {
+      document.getElementById("authForm").classList.remove('slideInDown');
+      document.getElementById("authForm").classList.add('slideOutDown');
+      document.getElementById("authForm").style.display ="block";
+      this.formOnScreen = false;
+    }
+
+   
+  }
+
+  displayLogIn()
+  {
+
+    if(!this.formOnScreen)
+    {
+      document.getElementById('logInForm').classList.remove("slideOutDown");
+      document.getElementById("logInForm").classList.add('slideInDown');
+      document.getElementById("logInForm").style.display ="block";
+      this.formOnScreen = true;
+    }
+
+    else
+    {
+      document.getElementById("logInForm").classList.remove('slideInDown');
+      document.getElementById("logInForm").classList.add('slideOutDown');
+      document.getElementById("logInForm").style.display ="block";
+      this.formOnScreen = false;
+    }
+
+   
   }
 
 }
