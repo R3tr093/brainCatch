756db6e414d7b3b6c1b9af22ac7140efaceaca21
Retro <mossiat.jeoffrey@outlook.com>
2019-10-25 01:40:15 +0200

    Add : put request

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ app.put("/api/users/update/:name", function(req, res) {
 
       var update = { score : req.body.score}; // 4
 
-      db.collection(USERS_COLLECTION).updateById(req.params.name, update, function(err) { // 5
+      db.collection(USERS_COLLECTION).update(req.params.name, update, function(err) { // 5
           if(err) {
               return res.send(500, err);
           }
