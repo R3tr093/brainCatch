24f99d634905e3ab39434c04a3a1c7c597f13da9
Retro <mossiat.jeoffrey@outlook.com>
2019-10-25 16:38:20 +0200

    Add : put request

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -96,7 +96,6 @@ export class UsersServicesService {
 
   updateFields(data){
 
-      console.log(this.userData[0].name)
       return this.http.put("https://braincatch.herokuapp.com/api/users/update",data,httpOptions,).subscribe(
         value => {
           console.log(value)
